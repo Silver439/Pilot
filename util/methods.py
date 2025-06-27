243d74b224e03c64385fcf7f14aeb376ec3b26ab
Silver439 <3482926637@qq.com>
2025-06-27 10:13:58 +0800

    new file:   Data/output.csv 	new file:   Data/rank_100.txt 	new file:   Data/rank_300.txt 	new file:   Data/rank_500.txt 	new file:   SAA_simulate.ipynb 	new file:   cluster.ipynb 	new file:   mixed_simulate.ipynb 	new file:   mult_SAA.ipynb 	new file:   mult_mixed.ipynb 	new file:   output.csv 	new file:   test_100.ipynb 	new file:   util/__pycache__/data_generate.cpython-311.pyc 	new file:   util/__pycache__/methods.cpython-311.pyc 	new file:   util/data_generate.py 	new file:   util/methods.py

---
+++ util/methods.py
@@ -0,0 +1,162 @@
+import numpy as np
+import copy
+from util.data_generate import Emin2
+from scipy.cluster import hierarchy
+
+def improvement(p,m,mu,std,R,Sigma):
+    choosen_index = [] #选定科目
+    remain_index = list(range(0,p)) #剩余科目
+    choosen_index.append(int(np.argmin(mu)))
+    remain_index.remove(np.argmin(mu))
+
+    for j in range(m-1):
+        best_improvement = -1 #全局最优
+        best_id = -1
+        for id1 in remain_index:
+            improvement_min = 100 #局部最小
+            for id2 in choosen_index:
+                temp = mu[id2]-Emin2(id1,id2,mu,std,R)
+                if temp < improvement_min:
+                    improvement_min = temp
+            if improvement_min > best_improvement:
+                best_improvement = improvement_min
+                best_id = id1
+        choosen_index.append(best_id)
+        remain_index.remove(best_id)
+
+    sub_Sigma = Sigma[np.ix_(choosen_index, choosen_index)]
+    B = np.linalg.cholesky(sub_Sigma) #对协方差矩阵进行cholesky分解
+    return B,choosen_index
+
+def SAA(p,m,mu,std,R,Sigma,J=1000):
+    B,submodular_index = improvement(p,2,mu,std,R,Sigma)
+    Gmin = 100
+    bestid = -1
+    for t in range(m-2):
+        for i in range(p):
+            if i not in submodular_index:
+                copy_index = copy.deepcopy(submodular_index)
+                copy_index.append(i)
+                sub_Sigma = Sigma[np.ix_(copy_index, copy_index)]
+                sub_B = np.linalg.cholesky(sub_Sigma) #对协方差矩阵进行cholesky分解
+                G_cul = 0
+                for j in range(J):
+                    z = np.random.randn(len(copy_index))
+                    temp = mu[copy_index] + sub_B @ z
+                    G_cul += np.min(temp)
+                G = G_cul/J
+                if G<Gmin:
+                    Gmin = G
+                    bestid = i
+        Gmin = 100
+        submodular_index.append(bestid)
+    sub_Sigma = Sigma[np.ix_(submodular_index, submodular_index)]
+    U = np.linalg.cholesky(sub_Sigma) 
+    return U,submodular_index
+
+def mixed(p,m,mu,std,R,Sigma,J=1000):
+    Gmin = 100
+    bestid = -1
+    J = J
+    B,mixed_index = improvement(p,2,mu,std,R,Sigma)
+    for j in range(m-2):
+        imp = []
+        for id1 in range(p):
+            if id1 not in mixed_index:
+                improvement_min = 100 #局部最小
+                for id2 in mixed_index:
+                    temp = mu[id2]-Emin2(id1,id2,mu,std,R)
+                    if temp < improvement_min:
+                        improvement_min = temp
+                imp.append(improvement_min)
+            else:
+                imp.append(0)
+        indices = np.argsort(imp)[-10:][::-1] #获取指标列表
+        for i in indices:
+            copy_index = copy.deepcopy(mixed_index)
+            copy_index.append(i)
+            sub_Sigma = Sigma[np.ix_(copy_index, copy_index)]
+            sub_B = np.linalg.cholesky(sub_Sigma) #对协方差矩阵进行cholesky分解
+            G_cul = 0
+            for j in range(J):
+                z = np.random.randn(len(copy_index))
+                temp = mu[copy_index] + sub_B @ z
+                G_cul += np.min(temp)
+            G_temp = G_cul/J
+            if G_temp < Gmin:
+                Gmin = G_temp
+                bestid = i
+        Gmin = 100
+        mixed_index.append(int(bestid)) 
+    sub_Sigma = Sigma[np.ix_(mixed_index, mixed_index)]
+    U = np.linalg.cholesky(sub_Sigma)    
+    return U,mixed_index
+
+def cluster_subjects(R, n_clusters=None, method='single'):
+    """
+    使用层次聚类对科目进行分组
+    
+    参数:
+    R - 相关系数矩阵 (p×p)
+    n_clusters - 期望的聚类数量(可选)
+    method - 链接方法: 'single', 'complete', 'average', 'ward'等
+    
+    返回:
+    聚类标签数组
+    """
+    # 将相关矩阵转换为距离矩阵(1-绝对相关系数)
+    distance_matrix = 1 - np.abs(R)
+    
+    # 执行层次聚类
+    linkage_matrix = hierarchy.linkage(distance_matrix, method=method)
+    
+    # 如果指定了聚类数量
+    if n_clusters is not None:
+        cluster_labels = hierarchy.fcluster(linkage_matrix, n_clusters, criterion='maxclust')
+    else:
+        # 或者使用距离阈值
+        cluster_labels = hierarchy.fcluster(linkage_matrix, 0.8, criterion='distance')
+    
+    # # 绘制树状图
+    # plt.figure(figsize=(12, 8))
+    # dendrogram = hierarchy.dendrogram(linkage_matrix, labels=np.arange(1, R.shape[0]+1))
+    # plt.title('Subject Clustering Dendrogram')
+    # plt.xlabel('Subject Index')
+    # plt.ylabel('Distance (1 - |correlation|)')
+    # plt.show()
+    
+    return cluster_labels
+
+def find_min_mu_in_clusters(mu, cluster_labels):
+    """
+    找出每个聚类中平均分(mu)最小的科目指标
+    
+    参数:
+    mu - 各科目平均分数组(形状为(p,))
+    cluster_labels - 聚类标签数组(形状为(p,))
+    
+    返回:
+    包含每类最小mu科目指标的列表
+    """
+    # 获取所有唯一的聚类标签
+    unique_clusters = np.unique(cluster_labels)
+    
+    # 存储每类中mu最小的科目指标
+    min_mu_indices = []
+    
+    for cluster in unique_clusters:
+        # 获取当前类别的所有科目索引
+        cluster_indices = np.where(cluster_labels == cluster)[0]
+        
+        # 获取这些科目的mu值
+        cluster_mu = mu[cluster_indices]
+        
+        # 找到mu最小的科目在cluster_indices中的位置
+        min_pos_in_cluster = np.argmin(cluster_mu)
+        
+        # 获取全局索引(在原始mu数组中的位置)
+        global_index = cluster_indices[min_pos_in_cluster]
+        
+        min_mu_indices.append(int(global_index))
+    
+    return min_mu_indices
